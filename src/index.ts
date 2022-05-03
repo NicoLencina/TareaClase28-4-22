93a6528b6bfd4b186a7389a2ac6ddac3f079e31b
Lencina Nicolas Angelo <104397441+NicoLencina@users.noreply.github.com>
2022-05-02 21:20:52 -0300

    Corregido

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-let datoBase = document.getElementById("datoBase");
-let datoAltura = document.getElementById("datoAltura");
-let btnArea = document.getElementById("btnArea");
+//defino las variables y acedo a HTML con "document.getElementById" llamandolo por su id
 
-btnArea.addEventListener("click", () => {
-  console.log("El area del rectangulo es", datoBase.value * datoAltura.value);
+let datoBase = document.getElementById("base");  
+let datoAltura = document.getElementById("altura");
+let buttonArea = document.getElementById("buttonArea");
+
+//vinculo clik con buton  y luego imprimo por consola
+
+buttonArea.addEventListener("click", () => {
+  console.log("El area del rectangulo es", (datoBase.value * datoAltura.value);
 });
